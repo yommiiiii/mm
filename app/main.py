fdd2c2cfe533e507b7e3ee066738a85cef23c0ca
yommiiiii <166799567+yommiiiiii@users.noreply.github.com>
2024-04-19 15:58:04 +0900

    ,

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=xxx&returnType=json"
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apikey=rQZQU7gH9q7V7lXi7838nH3lcU6BBE2jgj2AzSA5&returnType=json"
 
     contents = requests.get(URL).text
 
