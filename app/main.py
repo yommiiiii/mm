f4c6e9cd2ba74b5beee248657d8d4fb5b0401b9c
yommiiiii <166799567+yommiiiiii@users.noreply.github.com>
2024-04-19 15:44:30 +0900

    .

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI 
 
+import requests
+
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello Root"}
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=xxx&returnType=json"
+
+    contents = requests.get(URL).text
+
+    return {"message": contents}
 
 @app.get("/home")
 def home(): 
